c245fa239f720c7856b5627b710551fadc350f0c
汪月 <44462436+pickMswang@users.noreply.github.com>
2018-11-15 19:15:57 +0800

    小红书项目所包含的头文件

---
+++ head.h
@@ -0,0 +1,24 @@
+#include<mysql/mysql.h>
+#include <list>
+#include<string>
+#include<string.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/epoll.h>
+#include<iostream>
+#include <queue>
+#include <exception>
+#include <errno.h>
+#include <pthread.h>
+using namespace std;
