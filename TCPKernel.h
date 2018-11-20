a7a8f398be1eb5c2c71d7f7405b9d9c979e25c7d
汪月 <44462436+pickMswang@users.noreply.github.com>
2018-11-20 18:53:46 +0800

    服务器端中介者头文件

---
+++ TCPKernel.h
@@ -0,0 +1,60 @@
+
+#pragma once
+//#ifndef _TCPKERNEL_H
+//#define _TCPKERNEL_H
+#include "CMySql.h"
+#include "TCPNet.h"
+#include "thread_pool.h"
+#include "Packdef.h"
+class TCPKernel;
+class TCPNet;
+class CMySql;
+//class threadpool;
+typedef void (TCPKernel:: *PFUN)(int ,char*);
+struct ProtocolMap
+{
+	    PackType m_nType;
+		PFUN     m_pfun;
+};
+
+
+class TCPKernel
+{
+public:
+	TCPKernel();
+   virtual ~TCPKernel();
+public:
+	 bool Open() ;
+	 void Close() ;
+	 void DealData(int sockfd,char* szbuf) ;
+public:
+	void RegisterRq(int sockfd,char* szbuf);
+	void LoginRq(int sockfd,char* szbuf);
+	void PublishRq(int sockfd,char* szbuf);
+public:
+	TCPNet *m_pTcpNet;
+private:
+	CMySql m_sql;
+//	threadpool m_threadpool;
+
+
+};
+
+/*class RedItask
+{
+public:
+	    RedItask(int sockfd,TCPKernel*pKernel)
+		{
+			m_sockfd = sockfd;
+	
+			m_pKernel = pKernel;
+		}
+		~RedItask(){}
+		void RunItask();
+private:
+										
+		int  m_sockfd;											
+	    TCPKernel* m_pKernel;
+
+};*/
+//#endif
