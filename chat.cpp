9219fe3115166572267c8f4a30ba88a975634876
汪月 <44462436+pickMswang@users.noreply.github.com>
2018-11-24 23:21:57 +0800

    客户端聊天源文件

---
+++ chat.cpp
@@ -0,0 +1,19 @@
+#include "chat.h"
+#include "ui_chat.h"
+
+Chat::Chat(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Chat)
+{
+    ui->setupUi(this);
+}
+
+Chat::~Chat()
+{
+    delete ui;
+}
+
+void Chat::on_pushButton_2_clicked()
+{
+    close();
+}
