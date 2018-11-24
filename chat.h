19914918bd6281f3529e9f1ac7b408f9e6bbcb4b
汪月 <44462436+pickMswang@users.noreply.github.com>
2018-11-24 23:20:10 +0800

    Create chat.h

---
+++ chat.h
@@ -0,0 +1,25 @@
+#ifndef CHAT_H
+#define CHAT_H
+
+#include <QWidget>
+
+namespace Ui {
+class Chat;
+}
+
+class Chat : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Chat(QWidget *parent = 0);
+    ~Chat();
+
+private slots:
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::Chat *ui;
+};
+
+#endif // CHAT_H
