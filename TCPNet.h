73e18d071466500adffe7e978319d771494a250a
汪月 <44462436+pickMswang@users.noreply.github.com>
2018-12-06 10:20:18 +0800

    服务器网络头文件

--- TCPNet.h
+++ TCPNet.h
@@ -1,62 +1,87 @@
 #pragma once
 #include"head.h"
-#include "thread_pool.h"
-#include "TCPKernel.h"
- 
-#define MAX_EVENT 1024   //epoll_events的最大个数
-#define MAX_BUFFER 2048  //Buffer的最大字节
+#include"threadpool.h"
+#include"TCPKernel.h"
+
+#define MAX_EVENT 1024 //epoll_events 的最大个数
+
+#define MAX_BUFFER 2048 //buffer 最大字节
 class TCPKernel;
+class TCPNet;
+
+class BaseTask
+{
+public:
+	virtual void doit()=0;
+
+};
+class Task:public BaseTask
+{
+private:
+	int sockfd;
+    int repollfd;
+	TCPKernel* m_TCPKernel;
+public:
+	Task(int epollfd,int fd,TCPKernel*tcpKernel):sockfd(fd)
+	{ 
+		repollfd=epollfd;
+		m_TCPKernel =tcpKernel;
+	}
+	void doit();
+        void reset_oneshot(int repollfd,int fd){//重置事件
+	    epoll_event event;
+	    event.data.fd=fd;
+	    event.events=EPOLLIN|EPOLLET|EPOLLONESHOT;
+	    epoll_ctl(repollfd,EPOLL_CTL_MOD,fd,&event);
+	}
+        
+};
+
 class TCPNet
 {
 private:
-	bool is_stop;   //是否停止epoll_wait的标志
-	//int threadnum;   //线程数目
-	int sockfd;     //监听的fd
-	int port;      //端口
-	int epollfd;    //Epoll的fd
-public:
-    TCPKernel* m_pTCPKernel;
-	//threadpool<BaseTask> *pool;   //线程池的指针
-	//char address[20];
-	epoll_event events[MAX_EVENT];  //epoll的events数组
-	struct sockaddr_in bindAddr;   //绑定的sockaddr
- 
-public:
-	//构造函数
-	TCPNet(TCPKernel*tcpKernel);
+	bool is_stop;
+	int threadnum;
+	int sockfd;
+    int epollfd;
+	int port;
+
 	
-	//析构
-	virtual ~TCPNet(){}	
-
- 	//创建Socket 绑定 监听 创建Epoll
+public:
+	TCPKernel*m_pTCPKernel;
+	threadpool<BaseTask>*pool;
+	char address[20];
+	epoll_event events[MAX_EVENT]; 
+	struct sockaddr_in bindAddr;
+public:
+	TCPNet(TCPKernel*tcpKernel);
+	virtual ~TCPNet(){}
 	bool InitNetWork();
-
 	void UnInitNetWork();
- 	
 	void DealData();
-
-	bool SendData(int sockfd, char* szbuf, int nlen);	
- 
-	static int setnonblocking(int fd)  //将fd设置称非阻塞
+	bool SendData(int sockfd,char *buf,int nlen);
+	static int setnonblocking(int fd)  //将fd设置为非阻塞
 	{
-		int old_option = fcntl(fd, F_GETFL);
-		int new_option = old_option | O_NONBLOCK;
+		int old_option =fcntl(fd,F_GETFL);
+		int new_option=old_option|O_NONBLOCK;
 		fcntl(fd, F_SETFL, new_option);
 		return old_option;
 	}
- 
-	static void addfd(int epollfd, int sockfd, bool oneshot)  //向Epoll中添加fd
-	{//oneshot表示是否设置称同一时刻，只能有一个线程访问fd，数据的读取都在主线程中，所以调用都设置成false
+	static void addfd(int epollfd,int sockfd,bool oneshot)
+	{
 		epoll_event event;
-		event.data.fd = sockfd;
-		event.events = EPOLLIN | EPOLLET;
+		event.data.fd=sockfd;
+		event.events=EPOLLIN|EPOLLET;//epoll采用的边缘触发
 		if(oneshot)
 		{
-			event.events |= EPOLLONESHOT;
+			event.events|=EPOLLONESHOT;
 		}
-		epoll_ctl(epollfd, EPOLL_CTL_ADD, sockfd, &event); //添加fd
+		epoll_ctl(epollfd,EPOLL_CTL_ADD,sockfd,&event);
 		TCPNet::setnonblocking(sockfd);
 	}
 };
+
+
+
 
 
