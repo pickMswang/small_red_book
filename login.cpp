eabf0acbae23818952aff82637f0ed3ab73c8430
汪月 <44462436+pickMswang@users.noreply.github.com>
2018-12-11 22:11:34 +0800

    客户端登录源文件

--- login.cpp
+++ login.cpp
@@ -29,12 +29,21 @@ void Login::on_login_button_clicked()
     srr.m_nType = _DEF_PROTOCOL_LOGIN_RQ;
     sprintf(srr.m_szAccount,account.toStdString().c_str());
     sprintf(srr.m_szPassword,password.toStdString().c_str());
-
-    tcpKernel->SendData((char *)&srr,sizeof(srr));
+    //先发数据包头
+    STRU_HEADER shead;
+    shead.pac_size = sizeof(STRU_LOGIN_RQ);
+    while(tcpKernel->SendData((char *)&shead,sizeof(STRU_HEADER)) == -1)
+    {
+        Sleep(50);
+    }
+    //发实际数据包
+    while(tcpKernel->SendData((char *)&srr,sizeof(STRU_LOGIN_RQ)) == -1)
+    {
+        Sleep(50);
+    }
     HomePage* homepage=new HomePage(tcpKernel);
-     // hide();
-      homepage->show();
-
+    // hide();
+    homepage->show();
 }
 
 //注册
@@ -44,14 +53,34 @@ void Login::on_pushButton_clicked()
     QString account = ui->lineEdit->text();
     QString password = ui->lineEdit_2->text();
 
-    STRU_REGISTER_RQ srr;
+    for(int i=10;i<50;i++)
+    {
+         STRU_REGISTER_RQ srr;
+         srr.m_nType = _DEF_PROTOCOL_REGISTER_RQ;
+         sprintf(srr.m_szAccount,"%s%d",account.toStdString().c_str(),i);
+         sprintf(srr.m_szPassword,"%s",password.toStdString().c_str());
+         //发送数据包头
+         STRU_HEADER shead;
+         shead.pac_size = sizeof(STRU_REGISTER_RQ);
+         while(tcpKernel->SendData((char *)&shead,sizeof(shead)) == -1)
+         {
+             Sleep(50);
+         }
+         //发送实际数据报
+         while(tcpKernel->SendData((char *)&srr,sizeof(srr)) == -1)
+         {
+             Sleep(50);
+         }
+    }
+
+/*  STRU_REGISTER_RQ srr;
     srr.m_nType = _DEF_PROTOCOL_REGISTER_RQ;
     sprintf(srr.m_szAccount,account.toStdString().c_str());
     sprintf(srr.m_szPassword,password.toStdString().c_str());
 
-    tcpKernel->SendData((char *)&srr,sizeof(srr));
-    qDebug()<<"login";
-    qDebug()<<sizeof(srr);
+    tcpKernel->SendData((char *)&srr,sizeof(srr));*/
+   // qDebug()<<"login";
+    //qDebug()<<sizeof(srr);
 }
 
 void Login::ShowMsg(char* msg)
